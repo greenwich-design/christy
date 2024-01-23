92b5d6bee26ad456b35b25c433773228afcfa7dc
Darren Powell <hello@bigpepperdesign.com>
2024-01-23 12:01:13 +0000

    app update

--- src/js/bundle-app.js
+++ src/js/bundle-app.js
@@ -33,6 +33,55 @@ const bundleApp = {
             productBundleBlocker.classList.remove('!opacity-100', '!visible');
         }
 
+        function formatMoney(cents, format) {
+
+            let money_format = "£{{amount}}";
+            // ---------------------------------------------------------------------------
+            // Money format handler
+            // ---------------------------------------------------------------------------
+            if (typeof cents == 'string') { cents = cents.replace('.', ''); }
+            var value = '';
+            var placeholderRegex = /\{\{\s*(\w+)\s*\}\}/;
+            var formatString = (money_format);
+
+            function defaultOption(opt, def) {
+                return (typeof opt == 'undefined' ? def : opt);
+            }
+
+            function formatWithDelimiters(number, precision, thousands, decimal) {
+                precision = defaultOption(precision, 2);
+                thousands = defaultOption(thousands, ',');
+                decimal = defaultOption(decimal, '.');
+
+                if (isNaN(number) || number == null) { return 0; }
+
+                number = (number / 100.0).toFixed(precision);
+
+                var parts = number.split('.'),
+                    dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + thousands),
+                    cents = parts[1] ? (decimal + parts[1]) : '';
+
+                return dollars + cents;
+            }
+
+            switch (formatString.match(placeholderRegex)[1]) {
+                case 'amount':
+                    value = formatWithDelimiters(cents, 2);
+                    break;
+                case 'amount_no_decimals':
+                    value = formatWithDelimiters(cents, 0);
+                    break;
+                case 'amount_with_comma_separator':
+                    value = formatWithDelimiters(cents, 2, '.', ',');
+                    break;
+                case 'amount_no_decimals_with_comma_separator':
+                    value = formatWithDelimiters(cents, 0, '.', ',');
+                    break;
+            }
+
+            return formatString.replace(placeholderRegex, value);
+        }
+
         async function fetchStep(url) {
             await fetch(`${url}`)
                 .then((response) => response.text())
@@ -354,8 +403,11 @@ const bundleApp = {
                 totalPrice = Math.round(totalPrice);
             }
 
-            let totalPriceFormatted = Shopify.formatMoney(totalPrice, window.money_format);
-            let totalCompareFormatted = Shopify.formatMoney(totalCompare, window.money_format);
+            //let totalPriceFormatted = Shopify.formatMoney(totalPrice);
+            //let totalCompareFormatted = Shopify.formatMoney(totalCompare);
+            let totalPriceFormatted = formatMoney(totalPrice);
+            let totalCompareFormatted = formatMoney(totalCompare);
+
 
             if (totalCompare > totalPrice) {
                 bundlePriceHtml = `<span class="flex gap-x-2 font-semibold ">
